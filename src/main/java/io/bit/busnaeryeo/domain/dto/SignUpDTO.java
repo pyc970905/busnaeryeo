4e63360986aed626bc9a42b6a4fd67aa08caabb6
BIT <okto12357@gmail.com>
2022-07-25 14:47:25 +0900

    not blank, not null, not empty 차이점 공부후 수정 계정관련은 Not Blank가 더 좋은거 같음

--- src/main/java/io/bit/busnaeryeo/domain/dto/SignUpDTO.java
+++ src/main/java/io/bit/busnaeryeo/domain/dto/SignUpDTO.java
@@ -4,23 +4,29 @@ import io.bit.busnaeryeo.domain.entity.User;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotBlank;
+
 import javax.validation.constraints.Pattern;
 import java.util.Collections;
 
 @Getter
 @Setter
 public class SignUpDTO {
-    @NotEmpty(message = "이메일은 필수 입력값입니다.")
+    @NotBlank(message = "이메일은 필수 입력값입니다.")
     @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식에 맞지 않습니다.")
     private String username;
 
-    @NotEmpty(message = "비밀번호는 필수 입력값입니다.")
+    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String password;
+
+    @NotBlank(message = "필수입력 값입니다.")
     private String realName;
+    @NotBlank(message = "필수입력 값입니다.")
     private String gender;
+    @NotBlank(message = "필수입력 값입니다.")
     private String address;
+    @NotBlank(message = "필수입력 값입니다.")
     private String birthDay;
 
 
